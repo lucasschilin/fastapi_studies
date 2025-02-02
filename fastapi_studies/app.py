265ee65d7ffa84512db9edca557bd2a703beb5b3
Lucas Schlindwein <schilin.lucas@gmail.com>
2025-02-02 18:41:44 -0300

    Implementado manipulacao DB em todas as rotas

--- fastapi_studies/app.py
+++ fastapi_studies/app.py
@@ -1,7 +1,9 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
 
+from fastapi_studies.database import get_session
 from fastapi_studies.schemas.message import Message
 from fastapi_studies.schemas.user import UserList, UserPublic, UserSchema
 from fastapi_studies.services.users import (
@@ -16,40 +18,27 @@ app = FastAPI()
 
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    try:
-        return service_create_user(user)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+def create_user(body: UserSchema, session: Session = Depends(get_session)):
+    return service_create_user(body, session)
 
 
 @app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
-def get_users():
-    try:
-        return service_get_users()
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+def get_users(session: Session = Depends(get_session)):
+    return service_get_users(session)
 
 
 @app.get('/users/{id}/', status_code=HTTPStatus.OK, response_model=UserPublic)
-def get_user(id: int):
-    try:
-        return service_get_user(id)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+def get_user(id: int, session: Session = Depends(get_session)):
+    return service_get_user(id, session)
 
 
 @app.put('/users/{id}/', status_code=HTTPStatus.OK, response_model=UserPublic)
-def update_user(id: int, user: UserSchema):
-    try:
-        return service_update_user(id, user)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+def update_user(
+    id: int, body: UserSchema, session: Session = Depends(get_session)
+):
+    return service_update_user(id, body, session)
 
 
 @app.delete('/users/{id}/', status_code=HTTPStatus.OK, response_model=Message)
-def delete_user(id: int):
-    try:
-        return service_delete_user(id)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+def delete_user(id: int, session: Session = Depends(get_session)):
+    return service_delete_user(id, session)
--- fastapi_studies/services/users.py
+++ fastapi_studies/services/users.py
@@ -1,95 +1,115 @@
 from http import HTTPStatus
 
 from fastapi import HTTPException
-from sqlalchemy import create_engine, select
+from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 
 from fastapi_studies.models.user import User
-from fastapi_studies.schemas.user import UserDB, UserSchema
-from fastapi_studies.settings import Settings
-
-database = {}
-database_ids = []
+from fastapi_studies.schemas.user import UserSchema
 
 
-def service_create_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
-
-    with Session(engine) as session:
-        user_db = session.scalar(
-            select(User).where(
-                ((User.username == user.username) | (User.email == user.email))
-                & (User.deleted_at == None)
-            )
+def service_create_user(body: UserSchema, session: Session):
+    user = session.scalar(
+        select(User).where(
+            ((User.username == body.username) | (User.email == body.email))
+            & (User.deleted_at == None)
         )
-
-        if user_db:
-            if user.username == user_db.username:
-                raise HTTPException(
-                    status_code=HTTPStatus.CONFLICT,
-                    detail='Usuário não disponível.',
-                )
-            elif user.email == user_db.email:
-                raise HTTPException(
-                    status_code=HTTPStatus.CONFLICT,
-                    detail='Endereço de e-mail não disponível.',
-                )
-
-        user_db = User(
-            username=user.username, email=user.email, password=user.password
-        )
-
-        session.add(user_db)
-        session.commit()
-        session.refresh(user_db)
-
-    return user_db
-
-
-def service_get_users():
-    return {'users': []}
-
-
-def service_get_user(id: int):
-    engine = create_engine(Settings().DATABASE_URL)
-
-    with Session(engine) as session:
-        user_db = session.scalar(
-            select(User).where((User.id == id) & (User.deleted_at == None))
-        )
-
-        if not user_db:
-            raise HTTPException(
-                status_code=HTTPStatus.NOT_FOUND,
-                detail='Usuário não encontrado.',
-            )
-
-    return user_db
-
-
-def service_update_user(id: int, user: UserSchema):
-    if id not in database:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='Usuário não encontrado.',
-        )
-
-    user_with_id = UserDB(
-        id=id,
-        **user.model_dump(),
     )
-    database[id] = user_with_id
 
-    return user_with_id
+    if user:
+        if body.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Nome de usuário não disponível.',
+            )
+        elif body.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Endereço de e-mail não disponível.',
+            )
+
+    user = User(
+        username=body.username, email=body.email, password=body.password
+    )
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    return user
 
 
-def service_delete_user(id: int):
-    if id not in database:
+def service_get_users(session: Session):
+    users = session.scalars(select(User).where(User.deleted_at == None)).all()
+    return {'users': users}
+
+
+def service_get_user(id: int, session: Session):
+    user = session.scalar(
+        select(User).where((User.id == id) & (User.deleted_at == None))
+    )
+
+    if not user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND,
             detail='Usuário não encontrado.',
         )
 
-    database.pop(id, None)
+    return user
+
+
+def service_update_user(id: int, body: UserSchema, session: Session):
+    user = session.scalar(
+        select(User).where((User.id == id) & (User.deleted_at == None))
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Usuário não encontrado.',
+        )
+
+    other_user = session.scalar(
+        select(User).where(
+            ((User.username == body.username) | (User.email == body.email))
+            & (User.deleted_at == None)
+        )
+    )
+
+    if other_user:
+        if other_user.username == body.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Nome de usuário não disponível.',
+            )
+        elif other_user.email == body.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Endereço de e-mail não disponível.',
+            )
+
+    user.username = body.username
+    user.email = body.email
+    user.password = body.password
+
+    session.commit()
+    session.refresh(user)
+
+    return user
+
+
+def service_delete_user(id: int, session: Session):
+    user = session.scalar(
+        select(User).where((User.id == id) & (User.deleted_at == None))
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='Usuário não encontrado.',
+        )
+
+    user.deleted_at = func.now()
+
+    session.commit()
 
     return {'message': 'Usuário deletado.'}
